7b08eb3269f314d4cb64461429c26597f017ac8c
Alif Rizqullah Maruf <alifrizqullahmaruf2003@mail.ugm.ac.id>
2024-01-13 21:46:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/iontophoresis/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.iontophoresis
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.iontophoresis.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+            finish()
+        }, SPLASH_DELAY)
+
+
+    }
+    companion object {
+        private const val SPLASH_DELAY = 2000L
+    }
+}
