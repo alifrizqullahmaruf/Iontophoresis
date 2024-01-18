c3d2e53c82fa29f9f12600df3801e80266a0ec53
Alif Rizqullah Maruf <alifrizqullahmaruf2003@mail.ugm.ac.id>
2024-01-18 20:42:52 +0700

    Auth Design part 1

--- app/src/main/java/com/example/iontophoresis/LoginActivity.kt
+++ app/src/main/java/com/example/iontophoresis/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.example.iontophoresis
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.iontophoresis.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            goToRegisterSignIn.setOnClickListener {
+                startActivity(Intent(this@LoginActivity, RegisterUserActivity::class.java))
+            }
+        }
     }
 }
