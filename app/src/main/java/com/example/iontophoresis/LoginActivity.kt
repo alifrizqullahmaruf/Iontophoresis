c8cb89f4362a2d7bb82536652b22306872e5e77b
Alif Rizqullah Maruf <alifrizqullahmaruf2003@mail.ugm.ac.id>
2024-01-19 11:01:07 +0700

    Auth firebase : pembuatan akun dan login, belum shareprefrence.

--- app/src/main/java/com/example/iontophoresis/LoginActivity.kt
+++ app/src/main/java/com/example/iontophoresis/LoginActivity.kt
@@ -3,18 +3,52 @@ package com.example.iontophoresis
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.iontophoresis.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private val auth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         with(binding){
+            // Pergi ke register page
             goToRegisterSignIn.setOnClickListener {
                 startActivity(Intent(this@LoginActivity, RegisterUserActivity::class.java))
+            }
+
+            // Pergi ke Home Page
+            buttonLogin.setOnClickListener {
+                val email = binding.emailInputText.text.toString()
+                val password = binding.passwordInputText.text.toString()
+
+                // Check if email is not empty
+                if (email.isEmpty()){
+                    Toast.makeText(this@LoginActivity, "Input email", Toast.LENGTH_SHORT).show()
+                }
+
+                // Check if password is not empty
+                if (email.isEmpty()){
+                    Toast.makeText(this@LoginActivity, "Input password", Toast.LENGTH_SHORT).show()
+                }
+
+                // Memanggil firebase untuk masuk dengan menggunakaan akun yang sudah di buat
+                auth.signInWithEmailAndPassword(email,password).addOnCompleteListener {task ->
+                    if (task.isSuccessful){
+                        Toast.makeText(this@LoginActivity, "Login Success", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this@LoginActivity, Home::class.java))
+
+                    }else{
+                        Toast.makeText(this@LoginActivity, "Login Failed", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+
             }
         }
     }
--- app/src/main/java/com/example/iontophoresis/RegisterUserActivity.kt
+++ app/src/main/java/com/example/iontophoresis/RegisterUserActivity.kt
@@ -1,11 +1,86 @@
 package com.example.iontophoresis
 
+import android.content.Intent
+import android.media.MediaPlayer.OnCompletionListener
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.iontophoresis.databinding.ActivityRegisterUserBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterUserActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterUserBinding
+
+    // Firebase Authentication instance (initialized later in onViewCreated)
+    lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_user)
+        binding = ActivityRegisterUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        mAuth = FirebaseAuth.getInstance()
+
+
+        with(binding){
+            buttonRegister.setOnClickListener {
+
+                val email = binding.emailInputText.text.toString()
+                val password = binding.passwordInputText.text.toString()
+
+                // Check if email is not empty
+                if (email.isEmpty()){
+                    Toast.makeText(this@RegisterUserActivity, "Input email", Toast.LENGTH_SHORT).show()
+                }
+
+                // Check if password is not empty
+                if (email.isEmpty()){
+                    Toast.makeText(this@RegisterUserActivity, "Input password", Toast.LENGTH_SHORT).show()
+                }
+
+                // Create a new user with email and password
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                    OnCompleteListener<AuthResult?>{ task->
+                        if (task.isSuccessful){
+
+                            val Name =binding.NameInputText.text.toString()
+                            val nickName =binding.nickNameInputText.text.toString()
+
+                            // Clear text in EditText fields after successful registration
+                            binding.NameInputText.text?.clear()
+                            binding.nickNameInputText.text?.clear()
+                            binding.emailInputText.text?.clear()
+                            binding.dateBirthInputText.text?.clear()
+                            binding.passwordInputText.text?.clear()
+                            binding.passwordInputVericicationText.text?.clear()
+
+                            Toast.makeText(this@RegisterUserActivity, "Account Created", Toast.LENGTH_SHORT)
+                                .show()
+                        }else{
+                            Toast.makeText(this@RegisterUserActivity, "Authentication failed", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+                startActivity(Intent(this@RegisterUserActivity,LoginActivity::class.java))
+            }
+
+            // Icon google click listener
+            iconSocialGoogle.setOnClickListener {
+                // Open Google URL in a browser
+                val google = "https://www.google.com/"
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(google))
+                startActivity(intent)
+            }
+
+            // Icon facebook click listener
+            iconSocialFacebook.setOnClickListener {
+                // Open Facebook URL in a browser
+                val facebook = "https://id-id.facebook.com/"
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(facebook))
+                startActivity(intent)
+            }
+        }
+
     }
 }
