cf8c50bda83f67e5ba931eaa0233d47880c1f5c3
Alif Rizqullah Maruf <alifrizqullahmaruf2003@mail.ugm.ac.id>
2024-02-02 17:27:54 +0700

    Bottom navbar tinggal tambahin fragment

---
+++ app/src/main/java/com/example/iontophoresis/Halaman_utama.kt
@@ -0,0 +1,12 @@
+package com.example.iontophoresis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Halaman_utama : AppCompatActivity() {
+//    Tambahin logic untuk bagian faragmentnya
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman_utama)
+    }
+}
